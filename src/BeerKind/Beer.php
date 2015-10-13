c73c188fcd9b684816ed20c4c03785b18d8a76af
luciano-jr <luciano@lucianojr.com.br>
2015-10-13 19:52:19 -0300

    Initial commit

---
+++ src/BeerKind/Beer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luciano
+ * Date: 13/10/15
+ * Time: 19:44
+ */
+
+namespace Bavarianlabs;
+
+
+interface Beer
+{
+
+}
---
+++ src/Harmonization.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luciano
+ * Date: 13/10/15
+ * Time: 19:43
+ */
+
+namespace Bavarianlabs;
+
+
+class Harmonization
+{
+
+}
